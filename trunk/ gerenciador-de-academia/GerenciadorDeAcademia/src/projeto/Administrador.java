961bdae66b86e7b2b767c4a4b43cc44539773bda
abimaeldesousamonteiro <abimaeldesousamonteiro@b4c2d29e-9479-323b-0600-7d3b55dc8b69>
2010-10-22 02:34:35 +0000

    Versão Final para o primeiro milistone (Commit feito por mim , porem modificações feitas por todos do grupo )

--- trunk/ gerenciador-de-academia/GerenciadorDeAcademia/src/projeto/Administrador.java
+++ trunk/ gerenciador-de-academia/GerenciadorDeAcademia/src/projeto/Administrador.java
@@ -1,47 +1,48 @@
 package projeto;
 
 public class Administrador {
-	
-	public String login;
-	public String senha;
-	final private String STRING_VAZIA = "";
-	
-	
-	public Administrador(){
-		
-		senha = "";
-		login = "adm";
-		
-	}
-	/**
-	 * troca a senha do administrador
-	 * @param novaSenha
-	 * @throws Exception
-	 */
-	public void setSenha(String novaSenha)throws Exception{
-		if (novaSenha == null || novaSenha.equals(STRING_VAZIA)) {
-			throw new Exception("Senha Invalida");
-		}
-		senha = novaSenha;
-	}
-	/**
-	 * 
-	 * @return
-	 * retorna senha
-	 */
-	public String getSenha(){
-		return senha;
-	}
-	/**
-	 * 
-	 * @return 
-	 * retorna login
-	 */
-	public String getLogin(){
-		return login;
-	}
-	  	
-    	
+        
+        public String login;
+        public String senha;
+        final private String STRING_VAZIA = "";
+        
+        
+        public Administrador(){
+                
+                senha = "";
+                login = "admin";
+                
+        }
+        /**
+         * troca a senha do administrador
+         * @param novaSenha
+         * @throws Exception
+         */
+        public void setSenha(String novaSenha)throws Exception{
+                if (novaSenha == null || novaSenha.equals(STRING_VAZIA)) {
+                        throw new Exception("Senha Invalida");
+                }
+                senha = novaSenha;
+        }
+        /**
+         * 
+         * @return
+         * retorna senha
+         */
+        public String getSenha(){
+                return senha;
+        }
+        /**
+         * 
+         * @return 
+         * retorna login
+         */
+        public String getLogin(){
+                return login;
+        }
+                
+        
 }
+
 	
 	
