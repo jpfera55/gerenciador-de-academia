6e2ec33200dcafbc207099493639095f127568c1
emanuelorigamista <emanuelorigamista@b4c2d29e-9479-323b-0600-7d3b55dc8b69>
2010-11-25 23:51:13 +0000

    Caso de uso 4 consertado e a nova classe editar treino

---  gerenciador-de-academia/GerenciadorDeAcademia/src/gui/ControladorPrincipal.java
+++  gerenciador-de-academia/GerenciadorDeAcademia/src/gui/ControladorPrincipal.java
@@ -20,17 +20,23 @@ public class ControladorPrincipal {
     public static JPanel cadastraExercicio;
     public static Administrador admin;
     public static Usuario usuarioLogado;
+    public static Usuario usuarioSendoEditado;
     public static List<Usuario> listaDeUsuarios ;
-    public static List<Aparelho> aparelhosSemExercicio ;
+    public static List<Aparelho> aparelhos ;
+    public static JPanel criaTreino;
     public static JPanel loginGeral;
     public static JPanel cadastraAparelho;
+    public static JPanel escolheExercicio;
+    public static JPanel editaExercicio;
     public static List<Exercicio> listaDeExercicio ;
     public static List<GrupoMuscular> gruposMusculares ;
     public static GrupoMuscular grupoMuscularExistente, novoGrupoMuscular;
+    public static Exercicio exercicioSendoEditado;
 
     public static void main(String[] args){
+
         listaDeUsuarios = new ArrayList<Usuario>();
-        aparelhosSemExercicio = new ArrayList<Aparelho>();
+        aparelhos = new ArrayList<Aparelho>();
         gruposMusculares = new ArrayList<GrupoMuscular>();
         listaDeExercicio = new ArrayList<Exercicio>();
         admin = new Administrador();
@@ -40,6 +46,8 @@ public class ControladorPrincipal {
         loginGeral = new LoginGeral();
         primeiroLogin = new PrimeiroLogin();
         cadastraAluno = new CadastraAluno();
+        escolheExercicio = new EscolheExercicio();
+        //editaExercicio = new EditaExercicio();
         cadastraExercicio = new CadastraExercicio();
         primeiroLogin.setVisible(true);
         procuraAluno = new ProcuraAluno();
